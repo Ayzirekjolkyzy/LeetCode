9017c8cd7fe3cadb03504db8e8ff60bb2cce3525
Ayzirekjolkyzy <152137593+Ayzirekjolkyzy@users.noreply.github.com>
2024-01-08 12:36:04 +0600

    Time: 12 ms (44.93%), Space: 35.7 MB (82.53%) - LeetHub

---
+++ 2778-sum-of-squares-of-special-elements/2778-sum-of-squares-of-special-elements.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int sumOfSquares(vector<int>& nums) {
+        int ret=0;
+        for(int i=0; i<nums.size(); i++) {
+            if(nums.size()%(i+1)==0) ret+=(nums[i]*nums[i]);
+        }
+        return ret;
+    }
+};
