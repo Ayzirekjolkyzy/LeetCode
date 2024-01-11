2b57f8344c97268c9b23164bf495ec64ff456dc6
Ayzirekjolkyzy <152137593+Ayzirekjolkyzy@users.noreply.github.com>
2024-01-11 22:22:41 +0600

    Time: 132 ms (34.64%), Space: 10.9 MB (35.10%) - LeetHub

---
+++ 1995-count-special-quadruplets/1995-count-special-quadruplets.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countQuadruplets(vector<int>& nums) {
+        int ret=0;
+    for(int i=0; i<nums.size()-3; i++) {
+        for(int j=i+1; j<nums.size()-2; j++) {
+            for(int k=j+1; k<nums.size()-1; k++) {
+                for(int m=k+1; m<nums.size(); m++) {
+                    if(nums[i]+nums[j]+nums[k]==nums[m]) ret++;
+                }
+            }
+        }
+    }
+    return ret;
+    }
+};
