411f7cb54e2e9feaf4cff3e14a97d5099254c470
Ayzirekjolkyzy <152137593+Ayzirekjolkyzy@users.noreply.github.com>
2024-01-14 14:37:35 +0600

    Time: 14 ms (68.95%), Space: 26.1 MB (78.25%) - LeetHub

---
+++ 2032-two-out-of-three/2032-two-out-of-three.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> twoOutOfThree(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3) {
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        sort(nums3.begin(), nums3.end());
+        set<int> ret; vector<int> ans;
+        for(int i=0; i<nums1.size(); i++) {
+            for(int j=0; j<nums2.size(); j++) {
+                if(nums1[i]==nums2[j]) ret.insert(nums1[i]);
+                }
+        }
+        for(int i=0; i<nums2.size(); i++) {
+            for(int j=0; j<nums3.size(); j++) {
+                if(nums2[i]==nums3[j]) ret.insert(nums2[i]);
+                }
+        }
+        for(int i=0; i<nums1.size(); i++) {
+            for(int j=0; j<nums3.size(); j++) {
+                if(nums1[i]==nums3[j]) ret.insert(nums1[i]);
+                }
+        }
+        for(auto it: ret) {
+            ans.push_back(it);
+        }
+        return ans;
+    }
+};
