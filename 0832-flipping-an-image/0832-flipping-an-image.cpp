e91dfba71302a8e382f4dbf3a5f8d0aff1501b8e
Ayzirekjolkyzy <152137593+Ayzirekjolkyzy@users.noreply.github.com>
2024-01-15 23:30:39 +0600

    Time: 7 ms (33.90%), Space: 9.2 MB (26.29%) - LeetHub

--- 0832-flipping-an-image/0832-flipping-an-image.cpp
+++ 0832-flipping-an-image/0832-flipping-an-image.cpp
@@ -4,8 +4,7 @@ public:
         vector<vector<int>> ret(image.size(), vector<int>(image[0].size()));
         for(int i=0; i<image.size(); i++) {
             for(int j=image[i].size()-1, a=0; j>=0; a++, j--) {
-                if(image[i][j]==0) ret[i][a]=1;
-                else ret[i][a]=0;
+               ret[i][a]=image[i][j]^1;
             }
         }
         return ret;
