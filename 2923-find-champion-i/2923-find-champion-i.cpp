8accab14c267179047cc0f4b8c7febfcbbac6e5b
Ayzirekjolkyzy <152137593+Ayzirekjolkyzy@users.noreply.github.com>
2024-01-16 21:12:09 +0600

    Time: 67 ms (25.95%), Space: 36.6 MB (91.98%) - LeetHub

---
+++ 2923-find-champion-i/2923-find-champion-i.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findChampion(vector<vector<int>>& grid) {
+        bool k=true; int ret;
+        for(int i=0; i<grid.size(); i++) { 
+            k=true;
+            for(int j=0; j<grid[i].size(); j++) {
+                if(i==j) continue;
+                else {
+                   if(grid[i][j]==0) k=false; 
+                 }
+            }
+            if(k) {  ret=i; break; } 
+        }
+        return ret;
+    }
+};
