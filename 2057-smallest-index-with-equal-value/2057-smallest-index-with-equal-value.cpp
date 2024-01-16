1004de9c2bb9071d9fb030908c94eb86786f0a51
Ayzirekjolkyzy <152137593+Ayzirekjolkyzy@users.noreply.github.com>
2024-01-17 00:38:54 +0600

    Time: 7 ms (78.83%), Space: 22.2 MB (78.55%) - LeetHub

--- 2057-smallest-index-with-equal-value/2057-smallest-index-with-equal-value.cpp
+++ 2057-smallest-index-with-equal-value/2057-smallest-index-with-equal-value.cpp
@@ -3,9 +3,9 @@ public:
     int smallestEqual(vector<int>& nums) {
         int i=0;
         for(i=0; i<nums.size(); i++) {
-            if(i%10==nums[i]) break;
+            if(i%10==nums[i]) return i;
         }
-        if(i<nums.size()) return i;
+        
         return -1;
     }
 };
