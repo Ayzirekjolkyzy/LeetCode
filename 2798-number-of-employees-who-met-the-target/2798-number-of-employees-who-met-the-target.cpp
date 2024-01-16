6f4b68605640586e53afc43cff9f3073daeb8497
Ayzirekjolkyzy <152137593+Ayzirekjolkyzy@users.noreply.github.com>
2024-01-17 00:32:48 +0600

    Time: 0 ms (100.00%), Space: 20.8 MB (65.56%) - LeetHub

--- 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.cpp
+++ 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
     int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) {
-        sort(hours.begin(), hours.end()); 
-        int i=0;
-        for(i=0; i<hours.size(); i++) {
-            if(hours[i]>=target) break;
-            }
-        i=hours.size()-i;
-    return (i>0)? i:0;
-    }
+        int ans=0;
+       for(auto it: hours) {
+           if(it>=target) ans++;
+       }
+        return ans; }
 };
