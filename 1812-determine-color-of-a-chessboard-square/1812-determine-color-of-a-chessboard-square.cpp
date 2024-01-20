0e6ee99ada5fd7e31cacc41df58658af87d52ce4
Ayzirekjolkyzy <152137593+Ayzirekjolkyzy@users.noreply.github.com>
2024-01-21 01:40:57 +0600

    Time: 2 ms (46.65%), Space: 7.2 MB (13.86%) - LeetHub

--- 1812-determine-color-of-a-chessboard-square/1812-determine-color-of-a-chessboard-square.cpp
+++ 1812-determine-color-of-a-chessboard-square/1812-determine-color-of-a-chessboard-square.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool squareIsWhite(string c) {
-       return ((int(c[0])+int(c[1]))%2)? true:false;
+        if((int(c[0])+int(c[1]))%2) return true;
+        return false;
     }
 };
