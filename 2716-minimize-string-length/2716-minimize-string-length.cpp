26428ef5e1ada28d13de32d2cc4e3a3aabf0acc9
Ayzirekjolkyzy <152137593+Ayzirekjolkyzy@users.noreply.github.com>
2024-01-25 00:36:30 +0600

    Time: 7 ms (98.02%), Space: 12.4 MB (73.70%) - LeetHub

--- 2716-minimize-string-length/2716-minimize-string-length.cpp
+++ 2716-minimize-string-length/2716-minimize-string-length.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     int minimizedStringLength(string s) {
-   set<char> ret;
+  vector<bool> temp(26,0); int ret=0;
         for(int i=0; i<s.size(); i++) {
-            ret.insert(s[i]);
+            if(!temp[s[i]-'a']) ret++;
+            temp[s[i]-'a']=true;
         }
-        return ret.size();
+        return ret;
     }
 };
