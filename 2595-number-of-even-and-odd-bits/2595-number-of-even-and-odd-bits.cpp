62bc023061dab273defc3fecd5a683d7eb66eb56
Ayzirekjolkyzy <152137593+Ayzirekjolkyzy@users.noreply.github.com>
2024-01-27 23:22:05 +0600

    Time: 3 ms (60.14%), Space: 9 MB (7.90%) - LeetHub

--- 2595-number-of-even-and-odd-bits/2595-number-of-even-and-odd-bits.cpp
+++ 2595-number-of-even-and-odd-bits/2595-number-of-even-and-odd-bits.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     vector<int> evenOddBit(int n) {
-        string s="";
-        while(n>0) {
-            if(n%2) s+='1';
-            else s+='0';
-            n/=2;
+        int e=0, o=0;
+        while(n) {
+            if(n&1) e++;
+            n>>=1;
+            if(n==0) break;
+            if(n&1) o++;
+            n>>=1;
         }
-        vector<int> ret(2,0);
-        for(int i=0; i<s.size(); i++) {
-            if(i%2==0&&s[i]=='1') ret[0]++;
-            if(i%2&&s[i]=='1') ret[1]++;
-        }
-        return ret;
+        return {e,o};
     }
 };
