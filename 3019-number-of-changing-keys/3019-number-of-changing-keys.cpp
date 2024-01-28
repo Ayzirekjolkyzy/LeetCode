27ac112c363caaa6498a437c2bf12cbab8cb3b08
Ayzirekjolkyzy <152137593+Ayzirekjolkyzy@users.noreply.github.com>
2024-01-28 23:33:33 +0600

    Time: 0 ms (100.00%), Space: 8.1 MB (25.00%) - LeetHub

--- 3019-number-of-changing-keys/3019-number-of-changing-keys.cpp
+++ 3019-number-of-changing-keys/3019-number-of-changing-keys.cpp
@@ -3,7 +3,7 @@ public:
     int countKeyChanges(string s) {
         int ret=0;
         for(int i=0; i<s.size()-1; i++) {
-            if((abs(s[i]-s[i+1])!=32)&&(s[i]-s[i+1]!=0)) ret++;
+            if((abs(s[i]-s[i+1])!=32)&&(s[i]!=s[i+1])) ret++;
         }
         return ret;
     }
