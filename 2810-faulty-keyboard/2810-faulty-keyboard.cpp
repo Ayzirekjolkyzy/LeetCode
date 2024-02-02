4cb698423abb230187c9e762f450df6aa9c3a28c
Ayzirekjolkyzy <152137593+Ayzirekjolkyzy@users.noreply.github.com>
2024-02-02 22:34:31 +0600

    Time: 14 ms (8.69%), Space: 9.7 MB (13.58%) - LeetHub

---
+++ 2810-faulty-keyboard/2810-faulty-keyboard.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string finalString(string s) {
+        string ret="";
+        for(int i=0; i<s.size(); i++) {
+            if(s[i]=='i') {
+                reverse(ret.begin(), ret.end());
+            }
+            else ret+=s[i];
+        }
+        return ret;
+    }
+};
