9c126bdbc6eab969961e52c0ec832e95e270971e
Ayzirekjolkyzy <152137593+Ayzirekjolkyzy@users.noreply.github.com>
2024-02-19 00:01:27 +0600

    Time: 26 ms (5.74%), Space: 7.2 MB (40.00%) - LeetHub

--- 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
     int minOperations(int n) {
-       int ans=0;
-       for(int i=0;i<n/2;i++)
-       {
-           ans+=n-((2*i) + 1);
-       }
-       return ans;
+        int i=1, j;
+        int ret=0;
+        while(i<n){
+            j=i;
+            while(j<n){
+                ++j;
+                ret++;
+            }
+            i+=2;
+        }
+        return ret;
     }
 };
