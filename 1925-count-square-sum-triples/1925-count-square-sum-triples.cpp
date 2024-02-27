65514024edd5264f8b398edea348e5d122e40004
Ayzirekjolkyzy <152137593+Ayzirekjolkyzy@users.noreply.github.com>
2024-02-27 23:47:07 +0600

    Time: 235 ms (18.97%), Space: 7.1 MB (46.78%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
@@ -5,7 +5,7 @@ public:
         for(int i=1; i<=n; i++) {
             for(int j=1; j<=n; j++) {
                 for(int k=1; k<=n; k++) {
-                    if(pow(i,2)+pow(j,2)==pow(k,2)) 
+                    if((i*i)+(j*j)==(k*k)) 
                         s++;
                 }
             }
