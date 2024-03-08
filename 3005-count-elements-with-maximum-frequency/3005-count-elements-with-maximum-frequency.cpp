62e487744988cb5a57f347aa4add3046d5e55201
Ayzirekjolkyzy <152137593+Ayzirekjolkyzy@users.noreply.github.com>
2024-03-08 22:42:44 +0600

    Time: 7 ms (40.22%), Space: 21.8 MB (22.14%) - LeetHub

---
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxFrequencyElements(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<int> temp;
+        int t=nums[0], k=1;
+        for(int i=1; i<nums.size(); i++) {
+            if(t==nums[i]) k++;
+            else { temp.push_back(k); k=1; t=nums[i]; }
+        }
+        temp.push_back(k);
+        sort(temp.begin(), temp.end(), greater<int>());
+        t=temp[0]; k=temp[0]; int m=0;
+        for(int i=1; i<temp.size(); i++) {
+            if(temp[i]==t) k+=t;
+        }
+        return k;
+    }
+};
